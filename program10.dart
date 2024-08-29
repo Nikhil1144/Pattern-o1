0c8b0bfdbfaed8cf787022b11ad7af440b5413f2
Nikhil Deshmane <iamdeshmanenl@gmail.com>
2024-08-29 16:51:18 +0530

    Add files via upload

---
+++ program10.dart
@@ -0,0 +1,13 @@
+import"dart:io";
+void main(){
+  int row=int.parse(stdin.readLineSync()!);
+  for(int i=1;i<=row;i++){
+    int p=i;
+    for(int j=1;j<=row;j++){
+      stdout.write(" $p");
+      p++;
+    }
+    print("");
+    
+  }
+}
